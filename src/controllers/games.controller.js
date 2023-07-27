2d68bec94d2ea18e6b9a4dda55289cacd82df433
nathbalica <nathbalica@gmail.com>
2023-07-27 14:55:34 -0300

    feat: :sparkles: add search query string

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,10 +1,17 @@
 import { db } from "../database/database.connection.js";
 
 export async function getGames(req, res){
+    const name = req.query.name;
     try{
-        const games = await db.query('SELECT * FROM games')
-        res.status(200).send(games.rows)
-    }catch(err){
+        let games;
+        if (name) {
+            games = await db.query('SELECT * FROM games WHERE name ILIKE $1', [`${name}%`]);
+        } else {
+            games = await db.query('SELECT * FROM games');
+        }
+        
+        res.status(200).send(games.rows);
+    } catch(err) {
         res.status(500).send(err.message);
     }
 }
